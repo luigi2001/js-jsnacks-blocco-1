5396bd0c13e3de0162c9c3e9ce45a5f75e669f39
luigi2001 <luigitanga@virgilio.it>
2021-06-01 17:49:42 +0200

    correzione 4 esercizio

--- Snack/Snack4/js/script.js
+++ Snack/Snack4/js/script.js
@@ -11,7 +11,7 @@ var risposta = 'Mi dispiace purtroppo non sei stato invitato';
 
 while(i < invitati.length){
     if(invitati[i] == maiuscola){
-        console.log(risposta = 'Complimenti! Risulti tra gli invitati alla festa');
+        risposta = 'Complimenti! Risulti tra gli invitati alla festa';
     } else{
         console.log('non invitato');
     }
